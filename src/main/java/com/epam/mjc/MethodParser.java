e34820facb75a36a10c94c80ca8434f869ef2a68
d.anisimov <d.anisimov@ds-manager03.szspspb.local>
2023-06-15 19:34:33 +0300

    Not finished, need to work with.

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,8 +1,15 @@
 package com.epam.mjc;
 
-public class MethodParser {
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
 
-    /**
+import com.epam.mjc.MethodSignature.Argument;
+
+public class MethodParser {
+	
+	    /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
      *      1. access modifier - optional, followed by space: ' '
@@ -19,7 +26,30 @@ public class MethodParser {
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
-    public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+    public MethodSignature parseFunction(String signatureString) throws NullPointerException {
+    	StringTokenizer signatStringTokenizer = new StringTokenizer(signatureString, " ");
+    	String accessModifier;
+    	String returnType;
+    	if(signatStringTokenizer.countTokens() == 3) {
+    		accessModifier = signatStringTokenizer.nextToken();
+    		returnType = signatStringTokenizer.nextToken();
+    		String methodParamString = signatStringTokenizer.nextToken();
+    		String methodName = methodParamString[0];
+    		String[] argumentStrings = null;
+    		for(int i = 0; methodParamString.length < i-1;) {
+    			argumentStrings[i] = methodParamString[++i];
+    		}
+    		List<Argument> arguments= Arrays.asList(argumentStrings);
+    	}
+    	return new MethodParser();
     }
+    private static boolean hasMatchingAccessModifier(String str) {
+    	List<String> modifiers = Arrays.asList("public", "private", "protected");
+    	for(String modifier : modifiers) {
+    		if(str.contains(modifier)) {
+    			return true;
+    		}
+    	}
+    	return false;
+	}
 }
